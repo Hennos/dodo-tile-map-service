ca724eaf15b5dd8414a1de35166cf8745f336cca
Evgeny Karasev <gnomskg@gmail.com>
2020-08-21 19:25:13 +0300

    feat: add post robot_map action

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const jimp = require("jimp");
 
-router.get("/", function (req, res, next) {
+const router = express.Router();
+
+router.get("/", function (req, res) {
   res.send("Tile Map Service");
+});
+
+router.post("/robot_map", async function (req, res, next) {
+  const { map } = req.files;
+  const image = await jimp.read(map.data);
+  await image.write("./public/robot.jpg");
+  res.send("Add new map to server");
 });
 
 module.exports = router;
